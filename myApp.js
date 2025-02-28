236c943f8f66116df2f9d58f423ad3317e50b54a
Lens Kamdem <kamdem.lens@gmail.com>
2025-02-28 21:40:28 +0000

    Added basic securities with helmet as middleware on the express app

--- myApp.js
+++ myApp.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
+const helmet = require("helmet");
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+
+const ninetyDaysInSeconds = 90*24*60*60;
+
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"]}}));
+
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}));
 
 
 
@@ -49,6 +75,8 @@ const app = express();
 
 module.exports = app;
 const api = require('./server.js');
+const req = require('express/lib/request.js');
+const res = require('express/lib/response.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
